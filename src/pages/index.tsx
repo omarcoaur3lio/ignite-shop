75870ffa164abb7308da3b48e08765dd2ad543d7
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-06-21 08:35:08 -0300

    :sparkles: chore: Evitar prefetch dos links da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,6 +31,7 @@ export default function Home({ products }: HomeProps) {
             key={product.id}
             href={`/product/${product.id}`}
             className="keen-slider__slide"
+            prefetch={false}
           >
             <Image alt="" src={product.imageUrl} width={520} height={480} />
             <footer>
