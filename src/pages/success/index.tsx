d581f629f1e7afd12b9a06dfd695967d4f85e75a
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-06-22 20:28:00 -0300

    :construction: WIP: Adicioanar página de sucesso

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,3 +1,18 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+      <p>
+        Uhuul <strong>John Doe</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
