530d00d6d2da6d143b39c08be3d688d5341deb4d
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-06-25 23:28:02 -0300

    :sparkles: feat: Adicionar sidebar para mostrar conteúdo do carrinho de compras

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,29 +6,86 @@ import logoImg from "@/assets/logo.svg";
 import {
   CartButton,
   CartItemsAmount,
+  CartViewSideBar,
   Container,
   Header,
+  ImageContainer,
 } from "@/styles/pages/app";
 import Image from "next/image";
-import { Handbag } from "phosphor-react";
+import { Handbag, X } from "phosphor-react";
+import { useState } from "react";
 
 globalStyles();
 
 const itensAmount = 1;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [showSidebar, setShowSidebar] = useState("");
+
+  function handleShowSidebar() {
+    setShowSidebar("active");
+  }
+
+  function hideSidebar() {
+    if (showSidebar === "active") {
+      setShowSidebar("");
+    }
+  }
+
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
-        <CartButton>
+        <CartButton onClick={handleShowSidebar}>
           {itensAmount > 0 && <CartItemsAmount>{itensAmount}</CartItemsAmount>}
           <Handbag
             size={24}
-            className={String(itensAmount ? css({color: "$gray300"}) : css({color: "$gray500"}))}
+            className={String(
+              itensAmount
+                ? css({ color: "$gray300" })
+                : css({ color: "$gray500" })
+            )}
           />
         </CartButton>
       </Header>
+      <CartViewSideBar className={showSidebar}>
+        <X size={24} onClick={hideSidebar} />
+        <div className="cart-content">
+          <div>
+            <strong>Sacola de compras</strong>
+
+            <div className="productList">
+              {Array.from({ length: 12 }).map((_, index) => {
+                return (
+                  <div className="product" key={index}>
+                    <ImageContainer>
+                      <main key={index}>Product</main>
+                    </ImageContainer>
+                    <div>
+                      <span>Camiseta Beyond the Limits</span>
+                      <strong>R$ 79,90</strong>
+                      <button>Remover</button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <footer>
+            <div>
+              <span>Quantidade</span>
+              <span>2 itens</span>
+            </div>
+            <div>
+              <p>Valor total</p>
+              <strong>R$ 270,00</strong>
+            </div>
+
+            <button>Finalizar compra</button>
+          </footer>
+        </div>
+      </CartViewSideBar>
       <Component {...pageProps} />
     </Container>
   );
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -6,6 +6,9 @@ export const Container = styled("div", {
   alignItems: "flex-start",
   justifyContent: "center",
   minHeight: "100vh",
+
+  position: "relative",
+  overflow: "hidden",
 });
 
 export const Header = styled("header", {
@@ -31,7 +34,7 @@ export const CartButton = styled("button", {
 
   position: "relative",
 
-  transition: 'background 0.2s',
+  transition: "background 0.2s",
 
   "&:disabled": {
     opacity: 0.6,
@@ -50,7 +53,7 @@ export const CartItemsAmount = styled("span", {
   width: 24,
   height: 24,
   padding: "0.75rem",
-  borderRadius: '50%',
+  borderRadius: "50%",
   fontSize: "0.875rem",
   backgroundColor: "$green300",
   color: "$gray100",
@@ -61,5 +64,182 @@ export const CartItemsAmount = styled("span", {
   right: "-12px",
 
   fontWeight: "bold",
+});
 
+export const ImageContainer = styled("div", {
+  // height: 100,
+  width: 100,
+  background: "linear-gradient(180deg, #1EA483 0%, #7465D4 100%)",
+  borderRadius: 8,
+  padding: "0.25rem",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  img: {
+    objectFit: "cover",
+  },
+});
+
+export const CartViewSideBar = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+
+  gap: "1.5rem",
+
+  position: "absolute",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  width: 480,
+  padding: "3rem",
+
+  backgroundColor: "$gray800",
+  boxShadow: "-4px 0px 30px rgba(0, 0, 0, 0.8)",
+
+  zIndex: 2,
+
+  transform: "translateX(100%)",
+  transition: "transform 0.2s ease-in-out",
+
+  maxHeight: "100vh",
+
+  "&.active": {
+    transform: "translateX(0)",
+  },
+
+  svg: {
+    color: "$gray500",
+    position: "absolute",
+    top: 24,
+    right: 24,
+
+    cursor: "pointer",
+
+    transition: "color 0.2s",
+
+    "&:hover": {
+      color: "$gray100",
+    },
+  },
+
+  "div.cart-content": {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+
+    flex: 1,
+
+    strong: {
+      color: "$gray100",
+      fontSize: "$lg",
+      fontWeight: "bold",
+    },
+
+    div: {
+      display: "flex",
+      flexDirection: "column",
+    },
+
+    "div.productList": {
+      gap: "1.5rem",
+      marginTop: "2rem",
+      maxHeight: "calc(100vh - 450px)",
+
+      overflowY: "scroll",
+
+      "&::-webkit-scrollbar": {
+        width: "2px",
+      },
+      "&::-webkit-scrollbar-track": {
+        background: "$gray700",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        background: "$gray500",
+        borderRadius: "6px",
+      },
+
+      "div.product": {
+        display: "flex",
+        flexDirection: "row",
+        gap: "1.25rem",
+        color: "$gray300",
+
+        strong: {
+          fontSize: "$md",
+          marginTop: "0.25rem",
+        },
+
+        button: {
+          border: 0,
+          backgroundColor: "transparent",
+          cursor: "pointer",
+
+          marginTop: "0.5rem",
+          alignSelf: "flex-start",
+          color: "$green500",
+
+          fontSize: "1rem",
+          fontWeight: "bold",
+          lineHeight: 1.6,
+
+          transition: "color 0.2s",
+
+          "&:hover": {
+            color: "$green300",
+          },
+        },
+      },
+    },
+
+    footer: {
+      div: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "space-between",
+
+        marginTop: "0.5rem",
+        color: "$gray100",
+
+        span: {
+          color: "$gray300",
+        },
+
+        p: {
+          fontSize: "$md",
+          fontWeight: "bold",
+        },
+
+        strong: {
+          fontSize: "$xl",
+          fontWeight: "bold",
+        },
+      },
+
+      button: {
+        width: "100%",
+        backgroundColor: "$green500",
+        border: 0,
+        borderRadius: 8,
+        fontSize: "$md",
+        fontWeight: "bold",
+        color: "$white",
+        padding: "1.25rem",
+        marginTop: "3.375rem",
+        cursor: "pointer",
+        transition: "background-color 0.2s",
+
+        "&:disabled": {
+          opacity: 0.6,
+          cursor: "not-allowed",
+        },
+
+        "&:not([disabled]):hover": {
+          backgroundColor: "$green300",
+        },
+      },
+    },
+  },
 });
