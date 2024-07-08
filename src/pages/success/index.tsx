719fb33e2fd6a30f6e84b01987737ac01ae8bac9
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-07-08 09:01:00 -0300

    :sparkles: refactor: Modificar página de sucesso para compra de vários intens

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -4,16 +4,18 @@ import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 import Stripe from "stripe";
+import { useShoppingCart } from "use-shopping-cart";
 
 interface SuccessProps {
   customerName: string;
-  product: {
+  products: {
     name: string;
     imageUrl: string;
-  };
+  }[];
 }
-export default function Success({ customerName, product }: SuccessProps) {
+export default function Success({ customerName, products }: SuccessProps) {
   return (
     <>
       <Head>
@@ -23,13 +25,27 @@ export default function Success({ customerName, product }: SuccessProps) {
 
       <SuccessContainer>
         <h1>Compra efetuada!</h1>
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
-        <p>
-          Uhuul <strong>{customerName}</strong>, sua{" "}
-          <strong>{product.name}</strong> já está a caminho da sua casa.
-        </p>
+
+        <div className="productList">
+          {products.map((product) => (
+            <ImageContainer key={product.name}>
+              <Image src={product.imageUrl} width={120} height={110} alt="" />
+            </ImageContainer>
+          ))}
+        </div>
+
+        {products.length === 1 ? (
+          <p>
+            Uhuul <strong>{customerName}</strong>, sua{" "}
+            <strong>{products[0].name}</strong> já está a caminho da sua casa.
+          </p>
+        ) : (
+          <p>
+            Uhuul <strong>{customerName}</strong>, sua compra de{" "}
+            {products.length} camisetas já está a caminho da sua casa.
+          </p>
+        )}
+
         <Link href="/">Voltar ao catálogo</Link>
       </SuccessContainer>
     </>
@@ -53,15 +69,29 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   });
 
   const customerName = session.customer_details?.name;
-  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  // percorrer o array e salvar as imagens em um novo array
+  // const imageProducts = session.line_items.data.map((item) => {
+  //   const product = item.price.product as Stripe.Product;
+  //   return product.images[0];
+  // })
+
+  const products = session.line_items.data.map((item) => {
+    const product = item.price.product as Stripe.Product;
+    return {
+      name: product.name,
+      imageUrl: product.images[0],
+    };
+  });
+
+  // console.log(`products: `, products)
+
+  // const product = session.line_items.data[0].price.product as Stripe.Product;
 
   return {
     props: {
       customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0],
-      },
+      products,
     },
   };
 };
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -13,6 +13,15 @@ export const SuccessContainer = styled("main", {
     color: "$gray100",
   },
 
+  ".productList": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "4rem",
+    width: "100%",
+    paddingLeft: "10%",
+  },
+
   p: {
     fontSize: "$xl",
     color: "$gray300",
@@ -39,16 +48,18 @@ export const SuccessContainer = styled("main", {
 
 export const ImageContainer = styled("div", {
   width: "100%",
-  maxWidth: 130,
-  height: 145,
+  maxWidth: 140,
+  height: 140,
   background: "linear-gradient(180deg, #1EA483 0%, #7465D4 100%)",
-  borderRadius: 8,
+  borderRadius: "50%",
   padding: "0.25rem",
-  marginTop: "4rem",
+  marginLeft: "-10%",
 
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+
+  boxShadow: "0px 0px 60px rgba(0, 0, 0, 0.8)",
 
   img: {
     objectFit: "cover",
